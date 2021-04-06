435ecb9f54a9b6267eb2c089d56034faa1e7e007
Talisha White <>
2021-04-06 13:51:19 +0000

    my manifesto

---
+++ 0x0A-configuration_management.pp/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# this is a manifest
+file { 'holberton':
+  path    => '/tmp/holberton',
+  content => ' I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
